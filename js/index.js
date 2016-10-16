f517de3342bccf106a342ad2b37ab4d096eb67a4
cy <xiongcaihu@qq.com>
2016-10-16 13:57:19 +0800

    asdasdasdsa

--- js/index.js
+++ js/index.js
@@ -1,26 +1,52 @@
 (function() {
+    var count = 0;
 
     function init() {
         fixFooter();
         adjustContent();
         fixHeader();
+
+        disableScroll(document.body);
     }
 
     function fixFooter() {
         $(".footer").css("top", (window.innerHeight - 20 * 2.2));
-        $(".footer")[0].addEventListener('touchmove', function(event) {
-            event.preventDefault();
-        }, false);
+        disableScroll($(".footer")[0]);
     }
 
     function fixHeader() {
-        $(".header")[0].addEventListener('touchmove', function(event) {
-            event.preventDefault();
-        }, false);
+        disableScroll($(".header")[0]);
     }
 
     function adjustContent() {
-        $(".content").css("height", (window.innerHeight - 20 * (2.1 + 2.7))+"px");
+    	var startY;
+        $(".content").css("height", (window.innerHeight - 20 * (2.1 + 2.7)) + "px");
+        $(".content")[0].addEventListener('touchstart', function(event) {
+            enableScroll(document.body);
+            startY=event.touches[0].pageY;
+        }, false);
+        $(".content")[0].addEventListener('touchmove', function(event) {
+            // event.preventDefault();
+            var nowY=event.touches[0].pageY;
+            if($(".content").scrollTop()<=0 && nowY>=startY){
+            	disableScroll(document.body);
+            }
+        }, false);
+        $(".content")[0].addEventListener('touchend', function(event) {
+            disableScroll(document.body);
+        }, false);
+    }
+
+    function disableScroll(target) {
+        target.addEventListener('touchmove', temp, false);
+    }
+
+    function temp(event) {
+        event.preventDefault();
+    }
+
+    function enableScroll(target) {
+        target.removeEventListener('touchmove', temp);
     }
 
     window.app = {
